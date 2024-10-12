c62065243f0be6ab3fe01fab496cb739f7d88624
Chris <chris.a.lyman@gmail.com>
2024-10-12 07:13:41 -1000

    extra apps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Hookshot.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :ex_rated]
     ]
   end
 
